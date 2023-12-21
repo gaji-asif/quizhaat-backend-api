4714312517654fe1f015a5437d2f359807dd453e
jahidjd <jahidjd18@gmail.com>
2023-12-22 00:09:43 +0600

    :sparkles: add: new controller, models added for quiz API

---
+++ app/Models/QuestionBank.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QuestionBank extends Model
+{
+    use HasFactory;
+    protected $table = 'sm_question_banks';
+}
---
+++ app/Models/QuestionOptions.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QuestionOptions extends Model
+{
+    use HasFactory;
+    protected $table = 'sm_question_bank_mu_options';
+}
